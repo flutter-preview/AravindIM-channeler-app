3ecc6e7f4bcc6b1313a097ed92e39f058d73231b
AravindIM <git.aravindim@proton.me>
2023-07-03 00:17:30 +0530

    refactor(media-handler): image handler moves out

--- lib/widgets/media/media_handler.dart
+++ lib/widgets/media/media_handler.dart
@@ -1,8 +1,7 @@
 import 'package:channeler/widgets/media/flick_multi_player/flick_multi_manager.dart';
 import 'package:channeler/widgets/media/flick_multi_player/flick_multi_player.dart';
-import 'package:fast_cached_network_image/fast_cached_network_image.dart';
+import 'package:channeler/widgets/media/image_handler.dart';
 import 'package:flutter/material.dart';
-import 'package:insta_image_viewer/insta_image_viewer.dart';
 import 'package:mime/mime.dart';
 
 class MediaHandler extends StatelessWidget {
@@ -22,29 +21,7 @@ class MediaHandler extends StatelessWidget {
       builder: (context, constraints) {
         final size = constraints.maxWidth;
         final handler = switch (mediaType) {
-          "image" => InstaImageViewer(
-              child: FastCachedImage(
-                url: mediaUrl,
-                color: Colors.white,
-                colorBlendMode: BlendMode.dstOver,
-                fit: BoxFit.contain,
-                errorBuilder: (context, exception, stacktrace) {
-                  return Text(stacktrace.toString());
-                },
-                loadingBuilder: (context, progress) {
-                  return Center(
-                    child: SizedBox(
-                      height: 50,
-                      width: 50,
-                      child: CircularProgressIndicator(
-                        color: colorScheme.primary,
-                        value: progress.progressPercentage.value,
-                      ),
-                    ),
-                  );
-                },
-              ),
-            ),
+          "image" => ImageHandler(imageUrl: mediaUrl),
           "video" => FlickMultiPlayer(
               flickMultiManager: flickMultiManager,
               url: mediaUrl,
